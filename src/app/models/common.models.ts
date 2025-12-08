4c7ac4714d1a99c0688d3e9ba69c66f2a0d6b0ec
Carl <carl.cerf@gmail.com>
2025-12-08 23:49:03 +0100

    experience update

--- src/app/models/common.models.ts
+++ src/app/models/common.models.ts
@@ -2,3 +2,12 @@ export interface IOptions {
     label: string;
     value: string | number;
 }
+
+export interface IExperience {
+    company: string;
+    endDate: string;
+    startDate: string;
+    tasks: string[];
+    techs: string[];
+    title: string;
+}
--- src/app/sections/experience/experience.ts
+++ src/app/sections/experience/experience.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IExperience } from '../../models/common.models';
 
 @Component({
   selector: 'app-experience',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './experience.html',
   styleUrl: './experience.css',
 })
-export class Experience {
+export class Experience implements OnInit {
+  experiences: IExperience[] = [];
 
+  ngOnInit(): void {
+    this.experiences = [{
+      startDate: '09/2020',
+      endDate: "Aujourd'hui",
+      title: 'Développeur Web',
+      company: 'EIG',
+      tasks: [
+        "Développement d'une application web pour le secteur médical et médico-social : Dossier Unique de l'Usager, aide à la prescription, agenda, etc.",
+        "Conception et implémentation de nouvelles fonctionnalités, interfaces et services (Angular, Material, TypeScript).",
+        "Développement d'API back-end (4D).",
+        "Maintenance corrective et évolutive.",
+        "Rédaction de documentation technique et support interne.",
+        "Encadrement et accompagnement d'un alternant dans sesmissions.",
+        "Travail collaboratif avec Product Owner, UX/UI Designer et développeurs.",
+        "Utilisation d'outils de gestion de projet (Jira) et participation active aux cérémonies Agile (daily meeting, sprint planning)."
+      ],
+      techs: ['Angular', 'Typescript', 'Angular Material', 'HTML & SCSS']
+    }]
+  }
 }
