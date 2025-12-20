f7040b6d33b4dcfade4f388045c92705e20c37f6
Carl <carl.cerf@gmail.com>
2025-12-20 23:36:27 +0100

    active section update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { afterNextRender, Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { About } from './sections/about/about';
 import { Contact } from './sections/contact/contact';
 import { Experience } from './sections/experience/experience';
 import { Hero } from './sections/hero/hero';
 import { Projects } from './sections/projects/projects';
+import { activeSection } from './core/scroll-state';
 
 @Component({
   selector: 'app-root',
@@ -22,4 +22,31 @@ import { Projects } from './sections/projects/projects';
 })
 export class App {
   protected readonly title = signal('angular-portfolio');
+
+  constructor() {
+    // Ce bloc ne s'exécute QUE sur le client (navigateur)
+    afterNextRender(() => {
+      this.initScrollObserver();
+    });
+  }
+
+  private initScrollObserver() {
+    const observerOptions = {
+      root: null,
+      rootMargin: '-20% 0px -70% 0px',
+      threshold: 0
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          activeSection.set(entry.target.id);
+        }
+      });
+    }, observerOptions);
+
+    // Sélection des sections après le rendu
+    const sections = document.querySelectorAll('section[id]');
+    sections.forEach((section) => observer.observe(section));
+  }
 }
