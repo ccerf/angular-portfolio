33d633fac5c6a575f5c1806c05621ddd73c4e4ed
Carl <carl.cerf@gmail.com>
2025-12-29 19:52:26 +0100

    fix scroll to top when first link clicked

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DOCUMENT, inject, PLATFORM_ID } from '@angular/core';
 import { activeSection } from '../../core/scroll-state';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { IOptions } from '../../models/common.models';
 
 @Component({
@@ -15,11 +15,24 @@ export class Navbar {
   activeSection = activeSection;
   sections = [
     {value: 'about', label: 'À propos'},
-    {value: 'experience', label: 'Expérience'},
+    {value: 'experience', label: 'Expériences'},
     {value: 'projects', label: 'Projets'}
   ];
 
+  private document: Document = inject(DOCUMENT);
+  private platformId: Object = inject(PLATFORM_ID);
+
   navigateToSection(section: IOptions): void {
-    activeSection.set(section.value.toString())
+    activeSection.set(section.value.toString());
+    if (isPlatformBrowser(this.platformId)) {
+      if (section.value === this.sections[0].value) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      } else {
+        const element = this.document.getElementById(section.value.toString());
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    }
   }
 }
